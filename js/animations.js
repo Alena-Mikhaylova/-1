9ee6a374d88866c973a7c2cc8beb349383ec29da
Alena-Mikhaylova <alenamikhaylova1308@gmail.com>
2026-02-03 10:52:50 +0400

    Настроена анимация и прозрачность

--- js/animations.js
+++ js/animations.js
@@ -22,7 +22,7 @@ function initIncreaseNumberAnimation() {
   increaseNumberAnimationStep(0, element, 5000);
 }
 
-initIncreaseNumberAnimation();
+
 
 
 document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
@@ -44,3 +44,22 @@ document.querySelector('#budget').addEventListener('change', function handleSele
     document.querySelector('form').removeChild(otherInput)
   }
 });
+
+
+function updateScroll() {
+  if (window.scrollY > 0) {
+    document.querySelector('header').classList.add('header__scrolled');
+  } else {
+    document.querySelector('header').classList.remove('header__scrolled');
+  }
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+    if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation();
+  }
+}
+
+window.addEventListener('scroll', updateScroll);
+
+let animationInited = false;
